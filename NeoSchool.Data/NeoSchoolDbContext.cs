7dfd54798dbfa2d8cb314af33646b3e50d1e4c79
amavrov <alexandermavrov@mail.bg>
2019-07-02 15:31:28 +0300

    Addition to architecture commit

---
+++ NeoSchool.Data/NeoSchoolDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NeoSchool.Data
+{
+    public class NeoSchoolDbContext : IdentityDbContext
+    {
+        public NeoSchoolDbContext(DbContextOptions<NeoSchoolDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
